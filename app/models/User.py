1f73e84bbff902fe42222167651de4245c6d5541
Valérian de THEZAN de GAUSSAN <valerian.dethezandegaussan@monstock.net>
2022-02-13 21:36:18 +0100

    Avoid too-long hash error while inserting users.

--- app/models/User.py
+++ app/models/User.py
@@ -7,6 +7,6 @@ class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     roles = db.relationship('Role', secondary='user_roles')
