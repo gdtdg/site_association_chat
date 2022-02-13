265836da5f7b24f6c8306fef756fe890c1a6b612
Valérian de THEZAN de GAUSSAN <valerian.dethezandegaussan@monstock.net>
2022-02-13 21:53:26 +0100

    Fix delete function

--- app/app.py
+++ app/app.py
@@ -7,7 +7,6 @@ app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root:root@localhost:3306/assoc_chat'
 db = SQLAlchemy(app)
 
-
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
@@ -19,5 +18,5 @@ def db_insert(variable):
 
 
 def db_delete(variable):
-    db.session.db_delete(variable)
+    db.session.delete(variable)
     db.session.commit()
